8112c8bcebbe205bf803caa270b3d2dd40f10322
Eddie C. <edimc2000@gmail.com>
2026-01-23 20:35:41 -0600

    added xml doc

--- AppBuilder/AppBuilderController.cs
+++ AppBuilder/AppBuilderController.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿namespace ProductManagement.AppBuilder;
 
-namespace ProductManagement.AppBuilder;
-
+/// <summary>Application builder extensions for controller configuration.</summary>
 public static class AppBuilderController
 {
+    /// <summary>Configures ASP.NET Core controllers for the application.</summary>
+    /// <param name="builder">The WebApplicationBuilder to configure.</param>
     public static void ConfigureControllers (this WebApplicationBuilder builder)
     {
         builder.Services.AddControllers();
